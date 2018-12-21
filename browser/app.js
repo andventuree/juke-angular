44e8362d134269689af724b5bcf05a6b8050224a
andventuree <awwong92@gmail.com>
2018-12-21 17:35:16 -0500

    successfully completed load it

--- browser/app.js
+++ browser/app.js
@@ -1,50 +1,28 @@
-angular.module("juke", []).controller("mainController", [
+angular.module("juke", ["main"]);
+
+angular.module("main", []).controller("main", [
   "$scope",
   "$http",
   "$log",
   function($scope, $http, $log) {
-    var fakeAlbum = {
-      name: "Abbey Road",
-      imageUrl: "http://fillmurray.com/300/300",
-      songs: [
-        {
-          name: "Romeo & Juliette",
-          artists: [{ name: "Bill" }],
-          genre: "Funk",
-          audioUrl:
-            "https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3"
-        },
-        {
-          name: "White Rabbit",
-          artists: [{ name: "Bill" }, { name: "Bob" }],
-          genre: "Fantasy",
-          audioUrl:
-            "https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3"
-        },
-        {
-          name: "Lucy in the Sky with Diamonds",
-          artists: [{ name: "Bob" }],
-          genre: "Space",
-          audioUrl:
-            "https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3"
-        }
-      ]
-    };
-
-    $scope.album = fakeAlbum;
-
     $scope.getNames = function(artists) {
-      return artists.map(function(artist) {
+      return artists.map(artist => {
         return artist.name;
       });
     };
 
-    const albumsFromAPI = $http.get("/api/albums");
-
-    albumsFromAPI
-      .then(response => {
-        console.log("response from $http.get call to api/albums", response);
+    $http
+      .get("/api/albums")
+      .then(res => {
+        console.log(res.data);
+        return res.data;
       })
-      .catch($log.log);
+      .then(albums => $http.get(`/api/albums/${albums[0].id}`))
+      .then(res => res.data)
+      .then(album => {
+        album.imageUrl = `/api/albums/${album.id}/image`;
+        $scope.album = album;
+      })
+      .catch($log.error);
   }
 ]);
