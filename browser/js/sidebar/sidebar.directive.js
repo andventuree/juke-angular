b5b124d81ccbacfd380466f1fef0c7c05df7f24d
andventuree <awwong92@gmail.com>
2019-01-17 23:03:48 -0500

    completed sidebar section

---
+++ browser/js/sidebar/sidebar.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+
+juke.directive('sidebar', function(){
+
+   
+  //Directive Definition Object -- DDO
+  return {
+    restrict: '', //EA |very rare: CM
+    templateUrl: "/js/sidebar/sidebar.html", //when used, ng will AJAX req this file from server
+
+  }
+});
