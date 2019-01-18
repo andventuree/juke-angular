d98084af131b25f3af842934f01cf64d23b90662
andventuree <awwong92@gmail.com>
2019-01-17 23:47:42 -0500

    3rd way to use ctrl in custom directive

--- browser/js/player/player.directive.js
+++ browser/js/player/player.directive.js
@@ -1,10 +1,29 @@
 'use strict';
 
-juke.directive('player', function(){
+juke.directive('player', function(PlayerFactory){ //can inject dep in this fn, not the link fn
   return {
     restrict: 'E',
     templateUrl: '/js/player/player.html',
-    controller: 'PlayerCtrl' 
-    //to test if this way worked, comment out and note the btns on player bar at the bottom disappear
+    link: function (scope, element, attrs) { // link does everything ctrl does but more!
+      angular.extend(scope, PlayerFactory); // copy props from param2 to param1
+
+      scope.toggle = function () {
+        if ( PlayerFactory.isPlaying() ) PlayerFactory.pause();
+        else PlayerFactory.resume();
+      };
+    
+      scope.getPercent = function () {
+        return PlayerFactory.getProgress() * 100;
+      };
+    
+      scope.handleProgressClick = function (evt) {
+        PlayerFactory.seek(evt.offsetX / evt.currentTarget.scrollWidth);
+      };
+    },
+    // only reason to use this, if you share ctrl with another directive
+    // controller: 'PlayerCtrl' // require angular directives
   };
-})
+});
+
+// ** Advanced features of directives ** 
+// transclusion only works with a link fn
