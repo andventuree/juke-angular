dc725f841fe8eb09b106951ab665f37c2a280a1d
andventuree <awwong92@gmail.com>
2019-01-17 23:06:55 -0500

    formatting

--- browser/js/sidebar/sidebar.directive.js
+++ browser/js/sidebar/sidebar.directive.js
@@ -1,12 +1,9 @@
 'use strict';
 
-juke.directive('sidebar', function(){
-
-   
+juke.directive('sidebar', function(){   
   //Directive Definition Object -- DDO
   return {
     restrict: '', //EA |very rare: CM
     templateUrl: "/js/sidebar/sidebar.html", //when used, ng will AJAX req this file from server
-
   }
 });
