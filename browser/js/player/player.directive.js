abda8b517d01efdc58dcfd42371525ca1409e8ca
andventuree <awwong92@gmail.com>
2019-01-17 23:31:03 -0500

    2nd way to link up controller to player

--- browser/js/player/player.directive.js
+++ browser/js/player/player.directive.js
@@ -3,6 +3,8 @@
 juke.directive('player', function(){
   return {
     restrict: 'E',
-    templateUrl: '/js/player/player.html'
+    templateUrl: '/js/player/player.html',
+    controller: 'PlayerCtrl' 
+    //to test if this way worked, comment out and note the btns on player bar at the bottom disappear
   };
 })
